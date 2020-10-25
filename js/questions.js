a5775863b7b1abeb6338ee02e016c6766a22c837
Alexis Arando <lex4736@gmail.com>
2020-10-24 22:50:28 -0700

    Created Html, Css and Javascript pages

---
+++ js/questions.js
@@ -0,0 +1,33 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Which of the following is considered a reference datatype in Javascript?",
+  answer: "Function",
+  options: ["Boolean", "Function", "String","Integers"]
+},
+  {
+  numb: 2,
+  question: "Which of of the following should replace the question marks on this santhax <script ???? =script.js> so that it links to an external script?",
+  answer: "src",
+  options: ["rel", "href", "name", "src"]
+},
+  {
+  numb: 3,
+  question: "Which of the following synthax will display an alertbox using Javascript?",
+  answer: "alert(Bosskitty)",
+  options: ["alertbox(Bosskitty)", "msgbox(Bosskitty)", "msg(Bosskitty)", "alert(Bosskitty)"]
+},
+  {
+  numb: 4,
+  question: "Which of the following is a ternary oeprator in Javascript?",
+  answer: "?",
+  options: ["#", "::", "?", "&"]
+},
+  {
+  numb: 5,
+  question: "Which of the following is an example of a method for a javascript object?",
+  answer: "cat.stop()",
+  options: ["cat.stop()", "cat.color=white", "cat.weight=fat", "cat.name(Sir Chubster the 3rd)"]
+},
+];
